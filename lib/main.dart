51b399cae161f82676b2c5b2c3b3c5e0ea81888d
Natalya <nagitareigrashka@gmail.com>
2024-02-26 13:42:58 +0300

    Добавлена стартовая страница

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/pages/home.dart';
+import 'package:todolist/pages/main_screen.dart';
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(
     primaryColor: Colors.deepOrangeAccent,
   ),
-  home: const Home(),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => const MainScreen(),
+    '/todo': (context) => const Home(),
+  },
 ));
