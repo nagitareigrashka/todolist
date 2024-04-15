fe2adad8a133de7c397d4455b9d3829b668b9447
Natalya <nagitareigrashka@gmail.com>
2024-04-15 13:36:08 +0300

    firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todolist/pages/home.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   theme: ThemeData(
     primaryColor: Colors.deepOrangeAccent,
   ),
